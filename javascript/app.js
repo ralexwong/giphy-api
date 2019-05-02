273d45d820d37241f04b315851ec3ed85bcbe4e6
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-05-02 02:22:45 -0700

    changed some css. altered class and id names. polished some code. Still cant get the gifs to load though

--- javascript/app.js
+++ javascript/app.js
@@ -3,11 +3,14 @@ $(document).ready(function(){
     // array of seahawk players
     var players = ["kam chancellor", "russell wilson", "richard sherman", "marshawn lynch","pete carroll","earl thomas","bobby wagner","cliff avril","tyler lockett","doug baldwin"];
 
+    // create array to store new submissions
+    var userSubmissions = [];
+    
     // creates buttons from players array
     function intitialButtons() {
         for (var i = 0; i < players.length; i++){
             var gifButton = $("<button>");
-            gifButton.addClass("gif-button");
+            gifButton.addClass("btn btn-primary");
             gifButton.text(players[i]);
             $("#gif-list").append(gifButton);
         }
@@ -30,34 +33,65 @@ $(document).ready(function(){
             return;
         }
 
+        if (userSubmissions.includes(text)) {
+            return;
+        }
+
         else {
             
+        //adds a new button to the gif list 
         newButton.text(text);
-        gifButton.addClass("gif-button");
+        newButton.addClass("btn btn-primary");
         $("#gif-list").append(newButton);
 
+
+        userSubmissions.push(text)
         }
     });
 
-
-    $(this).on("click", function(){
+    // Event listener for our gif-button
+    $(".btn").on("click", function(){
         event.preventDefault();
 
         // empties last list of gifs
-        $("#gif-list").empty();
+        $("#gifs").empty();
+        console.log(this.textContent);
 
 
-        var button = $(this).val();
-        var queryURL = "https://api.giphy.com/v1/gifs/random?q=" + button + "&limit=10" + "&apikey=TuzC3cl7ZbXLtN3LyGrPq6YmOjZdWbzT";
+        var button = this.textContent;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + button + "&limit=10" + "&apikey=TuzC3cl7ZbXLtN3LyGrPq6YmOjZdWbzT";
 
-        // Creates AJAX call for the specific movie button being clicked
+        // Perfoming an AJAX GET request to our queryURL
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
 
-            var 
+            for (var i = 0; i < response.data.length; i++) {
+
+                var ratingDiv = $("<p>");
+                var rating = response.data[i].rating;
+
+                ratingDiv.addClass("rating");
+
+                $("#gifs").append(rating);
+
+                // grabs the gif url from the json
+                var gifURL = response.data[i].image_original_url;
+
+                // Creating and storing an image tag
+                var gifImage = $("<img>");
+
+                // Setting the gifImage src attribute to gifUrl
+                gifImage.attr("src", gifURL);
+                gifImage.attr("alt", "gif");
+
+                // Prepending the catImage to the images div
+                $("#gifs").append(gifImage);
+            }
+
+            
 
 
 
